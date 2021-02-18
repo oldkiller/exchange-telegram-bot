0dd3f7e1ae7cf0d5919f7fe860e4300fddee8e0e
Kira Krasnova <vaokyn@gmail.com>
2021-02-19 00:10:29 +0200

    Write function for extract data about a desired history period from a user's message for loading from API history data for making image
    Add bot command for sending history graph

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import datetime
 import os
+import random
 import re
+import uuid
 
+import matplotlib.pyplot as plt
 import telebot
 import requests
 
@@ -17,8 +21,8 @@ bot = telebot.TeleBot(telegram_token)
 
 def get_rates_list(currency: str) -> dict:
 	if db.check_data_out_of_date():
-		root = "https://api.exchangeratesapi.io/latest?base={}".format(currency)
-		raw_data = requests.get(root)
+		url = "https://api.exchangeratesapi.io/latest?base={}".format(currency)
+		raw_data = requests.get(url)
 		if raw_data.status_code != 200:
 			raise ConnectionError("Server unavailable")
 
@@ -61,6 +65,63 @@ def convert_currency(rates: dict, currency_to_convert: dict) -> dict:
 	return {"value": result, "currency": second_currency}
 
 
+def extract_history_data(msg_text: str) -> dict:
+	result_two_currency = re.search(r"[A-Z]{3}/[A-Z]{3}", msg_text).group(0)
+	if result_two_currency:
+		base, second = result_two_currency.split("/")
+	else:
+		result_second_currency = re.search(r"[A-Z]{3}", msg_text).group(0)
+		base, second = "USD", result_second_currency
+
+	days_str = re.search(r"[\d]+", msg_text).group(0)
+	days = int(days_str)
+
+	return {
+		"days": days,
+		"base": base,
+		"second": second
+	}
+
+
+def get_rates_history(data_for_history: dict) -> dict:
+	end_at = datetime.datetime.now().date()
+	start_at = end_at - datetime.timedelta(data_for_history["days"])
+
+	url = "https://api.exchangeratesapi.io/history"
+	params = {
+		"start_at": start_at,
+		"end_at": end_at,
+		"base": data_for_history["base"],
+		"symbols": data_for_history["second"]
+	}
+	req = requests.get(url, params=params)
+	if req.status_code != 200:
+		raise ConnectionError("Server unavailable")
+
+	raw_history_data = req.json()
+	history_data = raw_history_data["rates"]
+
+	return history_data
+
+
+def visualize_history(history: dict, msg_data: dict) -> str:
+	second = msg_data["second"]
+	list_of_date = list(history.keys())
+	list_of_date.sort()
+
+	rate_values = [round(history[date][second], 5) for date in list_of_date]
+	list_of_date = [date[5:] for date in list_of_date]
+
+	plt.figure(random.randint(1, 100000))
+	plt.plot(list_of_date, rate_values)
+	plt.grid()
+
+	filename = str(uuid.uuid1()) + ".png"
+	plt.savefig(filename)
+
+	return filename
+
+
 @bot.message_handler(commands=["list", "lst"])
 def send_rates_list(msg):
 	rates = get_rates_list("USD")
@@ -83,6 +144,41 @@ def send_converted_currency(msg):
 	reply_text = "{} {}".format(result["value"], result["currency"])
 
 	bot.send_message(msg.chat.id, reply_text)
+
+
+@bot.message_handler(commands=["history"])
+def send_history(msg):
+	msg_text = msg.text.split(" ", 1)[1]
+
+	data_for_history = extract_history_data(msg_text)
+
+	# Try load data from API
+	try:
+		history = get_rates_history(data_for_history)
+	except ConnectionError:
+		bot.send_message(msg.chat.id, "Server unavailable")
+		return
+
+	# If history data not available - inform user and stop function
+	if not history:
+		reply = "No exchange rate data is available for the selected currency"
+		bot.send_message(msg.chat.id, reply)
+		return
+
+	bot.send_chat_action(msg.chat.id, "upload_photo")
+
+	filename = visualize_history(history, data_for_history)
+
+	# Send user graph, if available
+	try:
+		photo = open(filename, "rb")
+	except:
+		bot.send_message(msg.chat.id, "Failed to send image")
+	else:
+		bot.send_photo(msg.chat.id, photo)
+		photo.close()
+	finally:
+		os.remove(filename)
 
 
 if __name__ == '__main__':
